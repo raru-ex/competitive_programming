b80c6505a7b51411ea35c383fe61290b2838a587
raru-ex <raru.okiraku@gmail.com>
2020-06-03 00:54:12 +0900

    add AtCoder ABC043 A

---
+++ src/main/AtCoder/ABC/043/A.scala
@@ -0,0 +1,14 @@
+// package ABC043
+
+// n!の計算
+object A extends App {
+  // val inputs: Seq[String] = scala.io.Source.stdin.getLines().toList
+  val inputs = Seq("5")
+  val num = inputs.head.toInt
+
+  println(solve(num))
+
+  def solve(num: Int): Int = {
+    (0 to num).foldLeft(0){ (acc, i) => acc + i }
+  }
+}
