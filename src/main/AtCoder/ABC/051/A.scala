c7bb6dc37d558aa7ad5afe34a4d8312bf8eeeaba
raru-ex <raru.okiraku@gmail.com>
2020-06-07 14:10:55 +0900

    add AtCoder ABC051 A

---
+++ src/main/AtCoder/ABC/051/A.scala
@@ -0,0 +1,7 @@
+// package ABC046
+
+object Main extends App {
+  // val inputs: Seq[String] = scala.io.Source.stdin.getLines().toList
+  val inputs = Seq("abcde,fghihgf,edcba")
+  println(inputs.head.split(",").mkString(" "))
+}
