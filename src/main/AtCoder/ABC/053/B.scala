fa3ad0c26a1130fb238c0290ebc32f3c54673082
raru-ex <raru.okiraku@gmail.com>
2020-06-21 12:09:39 +0900

    add AtCoder ABC53 B

---
+++ src/main/AtCoder/ABC/053/B.scala
@@ -0,0 +1,31 @@
+// package ABC046
+
+object Main extends App {
+  // val inputs: Seq[String] = scala.io.Source.stdin.getLines().toList
+  val inputs: Seq[String] = Seq("HASFJGHOGAKZZFEGA")
+  val s = inputs.head
+
+  println(solve(s))
+
+  private def solve(s: String): Int = {
+    var aIndex = -1
+    var zIndex = -1
+    val end = s.size - 1
+    (0 until end).foreach { i =>
+      s(i) match {
+        case 'A' if aIndex == -1 => aIndex = i
+        case _ => ()
+      }
+
+      s(end - i) match {
+        case 'Z' if zIndex == -1 => zIndex = end - i
+        case _ => ()
+      }
+
+      if(aIndex != -1 && zIndex != -1) {
+        return zIndex - aIndex + 1
+      }
+    }
+    return -1
+  }
+}
